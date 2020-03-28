745e3e2878ddf519b1edf6a5a29a1b215b88a81a
Anselmo Otero Neto <anselmo.neto@resourceit.com>
2020-03-28 17:14:02 -0300

    Adicionando algumas validações testes no backend.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
+const { errors } = require('celebrate');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 /**
  * Driver - Select * from users
  * Query builder - table('users').select('*').where(...); - ex KNEX.JS
  */
 
-app.listen(3333);
+//app.listen(3333);
+module.exports = app;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,19 +3,42 @@ const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
-
 const routes = express.Router();
+const { Segments, celebrate, Joi } = require('celebrate');
+
 
 routes.post('/sessions', SessionController.create);
 
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.create);
 routes.get('/ongs', OngController.index);
 
 routes.post('/incidents', IncidentController.create);
-routes.get('/incidents', IncidentController.index);
-routes.delete('/incidents/:id', IncidentController.delete);
 
-routes.get('/profile', ProfileController.index);
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), IncidentController.index);
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), IncidentController.delete);
+
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), ProfileController.index);
 
 // routes.post('/ongs', async (request, response) => {
 
