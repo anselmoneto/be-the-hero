0bf58539a7efb1e39c25a0c7e00aec02fb6c3c35
Anselmo Otero Neto <anselmo.neto@resourceit.com>
2020-03-28 01:43:32 -0300

    Implantação do projeto mobile com react native.

---
+++ mobile/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Routes from './src/routes';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+//import { Text, View } from 'react-native';
+//import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <Routes />
+    // <View>
+    //   <Text>Teste 123</Text>
+    // </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
